a9f2b58966bccf09579f8fb0141e1f4acb4c21a8
HP <HP@DESKTOP-N5QBMVO>
2021-09-03 22:10:38 +0530

    Add project files.

---
+++ LateBinding/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateBinding
+{
+
+    class Animal
+    {
+        public virtual void FoodHabits()
+        {
+            Console.WriteLine("Animals have different food  habits");
+        }
+    }
+
+    class Carnivorous : Animal
+    {
+        public override void FoodHabits()
+        {
+            base.FoodHabits();
+            Console.WriteLine("Carnivorous eat only meat");
+        }
+    }
+    class Implement
+    {
+        public void callFunction(Animal cr)
+        {
+            cr.FoodHabits();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance Demo!");
+            Implement im = new Implement();
+            Carnivorous cr = new Carnivorous();
+            // calling an object at runtime 
+            im.callFunction(cr);
+            Console.ReadKey();
+        }
+    }
+
+}
